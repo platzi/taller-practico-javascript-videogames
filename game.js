e1e57ad10ac7f7b3a89c1e4e04fa6dd063d4767b
Juan David Castro <me@juandc.co>
2022-05-30 12:53:37 -0500

    Events (septims clase con codigo)

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
@@ -41,10 +45,29 @@ function startGame() {
       game.fillText(emoji, posX, posY);
     });
   });
-  
-  // for (let row = 1; row <= 10; row++) {
-  //   for (let col = 1; col <= 10; col++) {
-  //     game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
-  //   }
-  // }
+}
+
+window.addEventListener('keydown', moveByKeys);
+btnUp.addEventListener('click', moveUp);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+btnDown.addEventListener('click', moveDown);
+
+function moveByKeys(event) {
+  if (event.key == 'ArrowUp') moveUp();
+  else if (event.key == 'ArrowLeft') moveLeft();
+  else if (event.key == 'ArrowRight') moveRight();
+  else if (event.key == 'ArrowDown') moveDown();
+}
+function moveUp() {
+  console.log('Me quiero mover hacia arriba');
+}
+function moveLeft() {
+  console.log('Me quiero mover hacia izquierda');
+}
+function moveRight() {
+  console.log('Me quiero mover hacia derecha');
+}
+function moveDown() {
+  console.log('Me quiero mover hacia abajo');
 }
