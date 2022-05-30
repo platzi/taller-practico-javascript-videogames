d81c5c634c8792b0fc61495eb60ebb827676b029
Juan David Castro <me@juandc.co>
2022-05-30 11:08:24 -0500

    Medidas canvas + innerElements (tercera clase)

--- game.js
+++ game.js
@@ -4,6 +4,31 @@ const game = canvas.getContext('2d');
 window.addEventListener('load', startGame);
 
 function startGame() {
+  let canvasSize;
+
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  const elementsSize = canvasSize / 10;
+
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
+  
+  // window.innerHeight
+  // window.innerWidth
+  
   // game.fillRect(0,50,100,100);
   // game.clearRect(50,50,50,50);
   // game.clearRect()
