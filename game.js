53639c4120c33ee8c2cf2f59f7d426477e2eb82b
Juan David Castro <me@juandc.co>
2022-05-30 10:46:57 -0500

    Intro a canvas (segunda clase)

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+  // game.fillRect(0,50,100,100);
+  // game.clearRect(50,50,50,50);
+  // game.clearRect()
+  // game.clearRect(0,0,50,50);
+
+  // game.font = '25px Verdana'
+  // game.fillStyle = 'purple';
+  // game.textAlign = 'center';
+  // game.fillText('Platzi', 25, 25);
+}
