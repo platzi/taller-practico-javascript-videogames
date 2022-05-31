3256379a751ec2c30c8d88c0bbf0d34173983006
Juan David Castro <me@juandc.co>
2022-05-31 10:06:57 -0500

    levelWin

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ const btnDown = document.querySelector('#down');
 
 let canvasSize;
 let elementsSize;
+let level = 0;
 
 const playerPosition = {
   x: undefined,
@@ -42,7 +43,13 @@ function startGame() {
   game.font = elementsSize + 'px Verdana';
   game.textAlign = 'end';
 
-  const map = maps[0];
+  const map = maps[level];
+
+  if (!map) {
+    gameWin();
+    return;
+  }
+  
   const mapRows = map.trim().split('\n');
   const mapRowCols = mapRows.map(row => row.trim().split(''));
   console.log({map, mapRows, mapRowCols});
@@ -85,7 +92,7 @@ function movePlayer() {
   const giftCollision = giftCollisionX && giftCollisionY;
   
   if (giftCollision) {
-    console.log('Subiste de nisvel!');
+    levelWin();
   }
 
   const enemyCollision = enemyPositions.find(enemy => {
@@ -99,6 +106,16 @@ function movePlayer() {
   }
 
   game.fillText(emojis['PLAYER'], playerPosition.x, playerPosition.y);
+}
+
+function levelWin() {
+  console.log('Subiste de nivel');
+  level++;
+  startGame();
+}
+
+function gameWin() {
+  console.log('¡Terminaste el juego!');
 }
 
 window.addEventListener('keydown', moveByKeys);
